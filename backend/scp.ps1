5b808b13ccbd0388367936490abc61f85118ca0d
Rodion Sharafiev <rodion12344321@mail.ru>
2022-12-08 00:25:18 +0300

    add upload script

--- backend/scp.ps1
+++ backend/scp.ps1
@@ -1,7 +1,9 @@
 scp -P 7682 -r .\common root@v1517237.hosted-by-vdsina.ru:~\backend
-scp -P 7682 -r .\configs\config.json root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\configs root@v1517237.hosted-by-vdsina.ru:~\backend
 scp -P 7682 -r .\static root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\db root@v1517237.hosted-by-vdsina.ru:~\backend
 scp -P 7682 -r .\templates root@v1517237.hosted-by-vdsina.ru:~\backend
 scp -P 7682 -r .\requirements.txt root@v1517237.hosted-by-vdsina.ru:~\backend
 scp -P 7682 -r .\server.py root@v1517237.hosted-by-vdsina.ru:~\backend
 scp -P 7682 -r .\wsgi.py root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\sup.ini root@v1517237.hosted-by-vdsina.ru:~\backend
