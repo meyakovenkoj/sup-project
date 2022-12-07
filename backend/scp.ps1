a0ee95583db70cad880aebbedd1e873f58ddfa99
Rodion Sharafiev <rodion12344321@mail.ru>
2022-12-07 21:06:45 +0300

    configs scripts

---
+++ backend/scp.ps1
@@ -0,0 +1,7 @@
+scp -P 7682 -r .\common root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\configs\config.json root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\static root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\templates root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\requirements.txt root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\server.py root@v1517237.hosted-by-vdsina.ru:~\backend
+scp -P 7682 -r .\wsgi.py root@v1517237.hosted-by-vdsina.ru:~\backend
